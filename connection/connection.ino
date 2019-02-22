a92bc8afc54d8049f0570ca38cbd6cb49a6ae8d5
Eduardo <mario.donjuan@uteq.edu.mx>
2019-02-22 14:21:48 -0600

    Create connection.ino
    first commit

---
+++ connection/connection.ino
@@ -0,0 +1,123 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+#define WLAN_SSID       "DIPLOMADO-CIC40"
+#define WLAN_PASS       ""
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME  "ilnetintel"
+#define AIO_KEY       "dffd998265cc4156a93bd680561bcf52"
+
+//HARDWARE
+#define LED_TEST 16
+#define BTN_FLASH 0
+#define LED_TX  2
+
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+
+/****************************** Feeds ***************************************/
+
+// Setup a feed called 'photocell' for publishing.
+// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
+Adafruit_MQTT_Publish button = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/button");
+
+// Setup a feed called 'onoff' for subscribing to changes.
+Adafruit_MQTT_Subscribe led = Adafruit_MQTT_Subscribe(&mqtt, AIO_USERNAME "/feeds/led");
+
+void MQTT_connect();
+bool flash;
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  pinMode(LED_TEST, OUTPUT);
+  digitalWrite(LED_TEST, !LOW);
+  pinMode(BTN_FLASH, INPUT);
+
+  Serial.println(F("Adafruit MQTT demo"));
+
+  // Connect to WiFi access point.
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+
+  // Setup MQTT subscription for onoff feed.
+  mqtt.subscribe(&led);
+}
+
+uint32_t x=0;
+
+void loop() {
+  MQTT_connect();
+
+  Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(5000))) {
+    if (subscription == &led) {
+      Serial.print(F("Got: "));
+      Serial.println((char *)led.lastread);
+
+      if(strcmp((char *)led.lastread, "ON") == 0)
+      {
+        digitalWrite(LED_TEST, !HIGH);
+      }
+      if(strcmp((char *)led.lastread, "OFF") == 0)
+      {
+        digitalWrite(LED_TEST, !LOW);
+      }     
+    }
+  }
+
+  // Notice MQTT button state
+  flash = !digitalRead(BTN_FLASH);
+  // Now we can publish stuff!
+  Serial.print(F("\nSending button val "));
+  Serial.print(flash);
+  Serial.print("...");  
+    
+  if (! button.publish(flash)) {
+    Serial.println(F("Failed"));
+  } else {
+    Serial.println(F("OK!"));
+  }
+}
+
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
