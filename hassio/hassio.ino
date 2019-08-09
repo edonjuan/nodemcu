d572ec858893a281add803dad6d6823202e7cce4
Eduardo Donjuan <mario.donjuan@uteq.edu.mx>
2019-08-09 08:46:52 -0500

    DIY
    Do It Yourself Challenge

--- hassio/hassio.ino
+++ hassio/hassio.ino
@@ -1,5 +1,7 @@
-// 2019 Refrigerator_v1.0
-// Author: edonjuan
+// DIY
+// Challenge
+// FLASH YOUR NODEMCU
+// TODO - CHANCE YOUR IP ADDRESS
 
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
@@ -9,17 +11,15 @@
 #define BTN_FLASH 0
 
 // Update these with values suitable for your network.
-const char* ssid = "Intel-IoT";
-const char* password = "1N73RN37D3L45C0545";
-const char* mqtt_server = "172.31.112.81";
-const char* mqtt_username = "mqtt_user";
-const char* mqtt_password = "mqtt_pass";
+const char* ssid = "DIPLOMADO-CIC40";
+const char* password = "";
+
+// TODO                    // YOUR IP
+const char* mqtt_server = "172.31.112.XX";
 
 // Objects
 WiFiClient espClient;
 PubSubClient client(espClient);
-
-
 
 void setup()
 {
@@ -32,9 +32,6 @@ void setup()
 
   pinMode(LED_TEST, OUTPUT);
   digitalWrite(LED_TEST, !LOW);
-
-  //Interrupts
-  attachInterrupt(digitalPinToInterrupt(BTN_FLASH), upload, CHANGE);
 }
  
 void loop()
@@ -42,7 +39,11 @@ void loop()
   if (!client.connected()) {
     reconnect();
   }
-  client.loop();
+  client.loop();  
+  digitalWrite(LED_TEST, !LOW);
+  upload();
+  delay(500);
+  digitalWrite(LED_TEST, !HIGH);
 }
 
 void setup_wifi() {
@@ -81,7 +82,11 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println(strMessage);
 
 
-  if(strMessage == "ON")  // BOOLEAN MESSAGES
+  if((strTopic == "topic")&&(strMessage == "ON"))
+  {
+     
+  }
+  if((strTopic == "topic")&&(strMessage == "OFF"))
   {
      
   }
@@ -90,14 +95,30 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void upload() {
   // Notice MQTT button state
   bool flash;
+  long randomnum;
   
   flash = !digitalRead(BTN_FLASH);
   // Now we can publish stuff!
   Serial.print(F("\nSending button val "));
   Serial.print(flash);
-  Serial.print("...");  
+  Serial.println("...");  
 
-  client.publish("ha/button", "ON"); 
+  if(flash)
+  {
+    client.publish("binary_sensors/mqtt/diy/01/state_topic", "ON"); 
+  }
+  else
+  {
+    client.publish("binary_sensors/mqtt/diy/01/state_topic", "OFF"); 
+  }
+
+  randomnum = random(0,100);
+  sendmqtt("sensors/mqtt/diy/01/state_topic", randomnum);
+
+  Serial.print(F("\nSending random val "));
+  Serial.print(randomnum);
+  Serial.println("..."); 
+  // TOPIC
 }
 
 void reconnect() {
@@ -105,10 +126,10 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("arduinoClient",mqtt_username,mqtt_password)) {
+    if (client.connect("arduinoClient")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      client.subscribe("ha/#");
+      client.subscribe("#");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -117,4 +138,24 @@ void reconnect() {
       delay(5000);
     }
   }
+}
+
+
+void sendmqtt(char* topic, float data)
+{
+  String data_str;
+  char data_chr[50];
+  int data_int;
+
+  data_int = (int)data;
+  if(data_int == data)
+  {
+    data_str = String(data_int);
+  }
+  else
+  {
+    data_str = String(data);
+  }
+  data_str.toCharArray(data_chr, data_str.length() + 1); 
+  client.publish(topic, data_chr);  
 }
